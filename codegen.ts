5187e421318cad8df5af6f72fda052f1ef177d8a
IgorEXE <werewolfofmoscow@yandex.ru>
2024-07-19 01:35:52 +0300

    feature/login_gql-codegen

---
+++ codegen.ts
@@ -0,0 +1,39 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "https://plan-backend-71h9.onrender.com/api/graphql",
+  documents: ["src/**/*.gql"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "src/shared/api/gql/graphql.ts": {
+      plugins: [
+        "typescript",
+        {
+          add: {
+            content: "/* eslint-disable */\n// prettier-ignore",
+          },
+        },
+      ],
+    },
+    "src/": {
+      preset: "near-operation-file-preset",
+      documents: ["src/**/*.gql"],
+      presetConfig: {
+        extension: ".generated.ts",
+        baseTypesPath: "shared/api/gql/graphql.ts",
+        folder: "../api",
+      },
+      config: {},
+      plugins: [
+        {
+          "typescript-operations": {
+            avoidOptionals: true,
+          },
+        },
+        "typed-document-node",
+      ],
+    },
+  },
+};
+
+export default config;
--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -2,17 +2,17 @@
 import { restClient } from "@/shared/api/base";
 import { useSessionState } from "../../entities/user/model";
 import { Button } from "@/shared/ui/design";
+import { useQuery } from "@tanstack/vue-query";
 
 const session = useSessionState();
 
-async function callAboba(){
-  await restClient.user.abobaUserAbobaPost()
+async function callAboba() {
+  await restClient.user.abobaUserAbobaPost();
 }
 </script>
 
 <template>
   <h1>Hello from dash🛹</h1>
-  <pre>{{ session.user }}</pre>
   <Button @click="() => session.logout()">logout</Button>
   <Button @click="() => callAboba()">Call aboba</Button>
 </template>
