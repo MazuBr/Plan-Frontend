a56885696b0165ba2426b262a8ae490e3c3b4e3c
IgorEXE <werewolfofmoscow@yandex.ru>
2024-08-08 15:36:31 +0300

    feature/calendar-linter-update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,36 @@
 // @ts-check
 
-import eslint from "@eslint/js"
+// import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 import vueParser from "vue-eslint-parser"
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  // eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.app.json",
+      },
+    },
+    ignores: ["*.generated.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+  {
+    files: ["**/*.vue"],
     languageOptions: {
       parser: vueParser,
       parserOptions: {
         sourceType: "module",
         parser: tseslint.parser,
+        extraFileExtensions: [".vue"],
       },
     },
     rules: {
